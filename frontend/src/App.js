b9670fb73aa03ba20a530321974d1e03948b225a
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2021-12-31 22:49:12 -0700

    ecomm api adjusted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
-    <Header/>
-    <main className='py-3'>
-      <Container>    <h1>Welcome to mern-ecom</h1>
-</Container>
-
-    </main>
-    <Footer/>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen></HomeScreen>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
