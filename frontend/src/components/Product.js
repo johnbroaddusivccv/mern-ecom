d9c0c7a17dffb57ae725ad3ca13c931df13396f2
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2022-01-01 00:22:52 -0700

    react router refactoring

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
     <>
       <Card className='my-3 p-3 rounded'>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Img src={product.image} variant='top' />
-        </a>
+        </Link>
 
         <Card.Body>
           <a href={`/product/${product._id}`}>
@@ -17,9 +19,10 @@ const Product = ({ product }) => {
           </a>
 
           <Card.Text as='div'>
-            <div className='my-3'>
-              {product.rating} from {product.numReviews} reviews
-            </div>
+            <Rating
+              value={product.rating}
+              text={` ${product.numReviews} reviews`}
+            />
           </Card.Text>
 
           <Card.Text as='h3'>${product.price}</Card.Text>
